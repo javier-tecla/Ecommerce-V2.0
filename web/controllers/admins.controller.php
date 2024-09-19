b97db56917ecf66e0819ceb466902cdae361a40f
javierBorjas <cristman11@gmail.com>
2024-09-18 22:57:55 -0300

    Validación lado del servidor

--- web/controllers/admins.controller.php
+++ web/controllers/admins.controller.php
@@ -12,55 +12,79 @@ class AdminsController
 
         if (isset($_POST["loginAdminEmail"])) {
 
-            $url = "admins?login=true&suffix=admin";
-            $method = "POST";
-            $fields = array(
+            if(preg_match('/^[.a-zA-Z0-9_]+([.][.a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/',$_POST["loginAdminEmail"])) {
 
-                "email_admin" => $_POST["loginAdminEmail"],
-                "password_admin" => $_POST["loginAdminPass"]
-            );
 
-            $login = CurlController::request($url, $method, $fields);
 
-            if ($login->status == 200) {
+                $url = "admins?login=true&suffix=admin";
+                $method = "POST";
+                $fields = array(
 
-                $_SESSION["admin"] = $login->results[0];
+                    "email_admin" => $_POST["loginAdminEmail"],
+                    "password_admin" => $_POST["loginAdminPass"]
+                );
 
-                echo '<script>
+                $login = CurlController::request($url, $method, $fields);
+
+                if ($login->status == 200) {
+
+                    $_SESSION["admin"] = $login->results[0];
+
+                    echo '<script>
                 
                     location.reload();
                 
                 </script>';
-            } else {
-
-                $error = null;
-
-                if ($login->results == "Wrong email") {
-
-                    $error = "Correo no existe";
                 } else {
 
-                    $error = "Contraseña incorrecta";
-                }
+                    $error = null;
 
-                if ($error) {
-                    echo '<div id="error-alert" class="alert alert-danger mt-3 text-center">' . $error . '</div>';
-                    echo '
+                    if ($login->results == "Wrong email") {
+
+                        $error = "Correo no existe";
+                    } else {
+
+                        $error = "Contraseña incorrecta";
+                    }
+
+                    if ($error) {
+                        echo '<div id="error-alert" class="alert alert-danger mt-3 text-center">' . $error . '</div>
+                        
                     <script>
-                        // Ejecuta una función después de 3 segundos
+                        // Ejecuta una función después de 5 segundos
                         setTimeout(function() {
-                            var errorAlert = document.getElementById("error-alert");
+                            let errorAlert = document.getElementById("error-alert");
                             if (errorAlert) {
                                 // Oculta el mensaje de error
                                 errorAlert.style.display = "none";
                             }
-                        }, 3000); // 3000 milisegundos = 3 segundos
+                        }, 3000); // 5000 milisegundos = 5 segundos
                 
                     fncFormatInputs();
                     
                 </script>
 
                 ';
+                    } else {
+
+                        echo '<div id="error-alert" class="alert alert-danger mt-3 text-center">' . $error . '</div>
+                        
+                    <script>
+                        // Ejecuta una función después de 5 segundos
+                        setTimeout(function() {
+                            var errorAlert = document.getElementById("error-alert");
+                            if (errorAlert) {
+                                // Oculta el mensaje de error
+                                errorAlert.style.display = "none";
+                            }
+                        }, 3000); // 5000 milisegundos = 5 segundos
+                
+                    fncFormatInputs();
+                    
+                </script>
+
+                ';
+                    }
                 }
             }
         }
