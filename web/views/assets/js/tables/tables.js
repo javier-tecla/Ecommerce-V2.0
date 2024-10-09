0fd918aaa3e114777ef7d4af44ecd72e189fd8b1
javierBorjas <cristman11@gmail.com>
2024-10-08 22:03:47 -0300

    DataTable configuracion Inicial

--- web/views/assets/js/tables/tables.js
+++ web/views/assets/js/tables/tables.js
@@ -1,1 +1,31 @@
-$("#tables").DataTable();
+$("#tables").DataTable({
+    "responsive":true,
+    "aLengthMenu":[[10, 25, 50, 100],[10, 25, 50, 100]],
+    "order":[[0, "desc"]],
+    "language":{
+
+        "sProcessing":      "Procesando...",
+        "sLengthMenu":      "Mostrar _MENU_ registros",
+        "aZeroRecords":     "No se encontraron resultados",
+        "sEmptyTable":      "Ningún dato disponible en esta tabla",
+        "sInfo":            "Mostrando registros del _START_ al _END_ de un total de _TOTAL_",
+        "sInfoEmpty":       "Mostrando registros del 0 al 0 de un total de 0",
+        "sInfoFiltered":    "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix":     "",
+        "sSearch":          "Buscar:",
+        "sUrl":             "",
+        "sInfoThousands":   ",",
+        "sLoadingRecords":  "Cargando...",
+        "oPaginate": {
+            "sFirst":    "Primero",
+            "sLast":     "Último",
+            "sNext":     "Siguiente",
+            "sPrevious": "Anterior",
+        },
+        "oAria": {
+        "oSortAscending":   ": Activar para ordenar la columna de manera ascendente",
+        "oSortDescending":   ": Activar para ordenar la columna de manera descendente"
+         }
+    }
+    
+});
